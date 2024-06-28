2e761adddb2397a7f44c4d1948e4acbb2a6318a4
Binxly <zbagley610@gmail.com>
2024-06-28 12:38:56 -0400

    new

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,4 +12,19 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
-require("lazy").setup("plugins")
+
+require("lazy").setup({
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- Optional
+      {
+        "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+        opts = {},
+      },
+    },
+    config = true
+  }
+})
