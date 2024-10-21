0e0d40a8c96a51b59f5b658b3a7ead65103b63dd
Zac <contact@binx.page>
2024-10-21 14:55:17 -0400

    Oct. 21 2024

--- polybar/.config/polybar/launch_polybar.sh
+++
@@ -1,7 +0,0 @@
-if type "xrandr" > /dev/null 2>&1; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
-else
-  polybar --reload toph &
-fi
